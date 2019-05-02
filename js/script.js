362f30adc60115d8d46e6fe00f7f161ec81685e6
Aleksandr Staetsky <myexploretime@gmail.com>
2019-05-02 23:43:57 +0300

    discount button color fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-$(document).ready(function(){
-    $('.stella-carousel__slides').slick({
-        infinite: true,
-        autoplay: true,
-    });
+function owlInitialize() {
+  if ($(window).width() < 768) {
+      $('.to_mobal').addClass("owl-carousel");
+      $('.owl-carousel').owlCarousel({
+          loop:true,    
+          margin: 5,  
+          nav: true,
+          navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+          dots: false,        
+          responsive:{
+              0:{
+                  items:1,           
+              },
+              480:{
+                  items:1,           
+              }
+            
+          }
+      });
+  }else{
+      $('.owl-carousel').owlCarousel('destroy');
+      $('.to_mobal').removeClass("owl-carousel");
+  }
+  }
+  $(document).ready(function(e) {
+  owlInitialize();
+  });
+  $(window).resize(function() {
+  owlInitialize();
   });
 
 //   burger menu setup 
@@ -19,11 +43,10 @@ $('.burger, .overlay').click(function(){
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
-          
 
 
-
-
-
-
+//   bootstrap
+$('.carousel').carousel({
+    interval: 2000
+  })
 
